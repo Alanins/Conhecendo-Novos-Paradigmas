b9e351840848b257bb9d68751d446665f301d8af
Liliane Moreira <106605093+Alanins@users.noreply.github.com>
2024-08-30 22:35:58 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,58 +1,62 @@
-import sys
-
 class Calculadora:
-    def __init__(self, valorA, valorB, operacao):
-        self.__valorA = valorA
-        self.__valorB = valorB
+    def __init__(self):
+        self.__valorA = 0
+        self.__valorB = 0
+        self.__operacao = ""
+
+    @property
+    def valorA(self):
+        return self.__valorA
+
+    @valorA.setter
+    def valorA(self, valor):
+        self.__valorA = valor
+
+    @property
+    def valorB(self):
+        return self.__valorB
+
+    @valorB.setter
+    def valorB(self, valor):
+        self.__valorB = valor
+
+    @property
+    def operacao(self):
+        return self.__operacao
+
+    @operacao.setter
+    def operacao(self, operacao):
         self.__operacao = operacao
 
-@property
-def valorA(self):
-    return self.__valorA
-@valorA.setter
-def valorA(self, valorA):
-    self.__valorA = valorA
+    def validarOperacao(self, simbolo):
+        operacoes_validas = ['+', '-', '*', '/']
+        return simbolo in operacoes_validas
 
-@property
-def valorB(self):
-    return self.__valorB
-@valorB.setter
-def valorB(self, valorB):
-    self.__valorB = valorB
+    def calcular(self):
+        if not self.validarOperacao(self.__operacao):
+            print("Operação inválida. O programa será encerrado.")
+            exit()
 
-@property   
-def operacao(self):
-    return self.__operacao
-@operacao.setter    
-def operacao(self, operacao):
-    self.__operacao = operacao
+        if self.__operacao == '+':
+            return self.__valorA + self.__valorB
+        elif self.__operacao == '-':
+            return self.__valorA - self.__valorB
+        elif self.__operacao == '*':
+            return self.__valorA * self.__valorB
+        elif self.__operacao == '/':
+            if self.__valorB == 0:
+                print("Erro: Divisão por zero. O programa será encerrado.")
+                exit()
+            return self.__valorA / self.__valorB
 
-def validarOperacao(simbolo):
-    validos = ['+', '-', '*', '/']
-    if simbolo in validos:
-        return True
-    else:
-        return False
-    
-def calcular(self):
-    if not self.validarOperacao():
-        print("Operação inválida")
-        sys.exit()
-        return
-    if self.__operacao == '+':
-        print(self.__valorA + self.__valorB)
-    elif self.__operacao == '-':
-        print(self.__valorA - self.__valorB)
-    elif self.__operacao == '*':
-        print(self.__valorA * self.__valorB)
-    elif self.__operacao == '/':
-        if self.__valorB == 0:
-            print("Erro: Divisão por zero")
-            sys.exit()
-            return
-        print(self.__valorA / self.__valorB)
-        return
-    
-def mostrarResultado(self):
-      print(str(self.valorA) + ' ' + self.operacao + ' ' + str(self.valorB) + ' = ' + str(self.calcular()))
-    
+    def mostrarResultado(self):
+        resultado = f"{self.__valorA} {self.__operacao} {self.__valorB} = {self.calcular()}"
+        print(resultado)
+
+    def entradaDados(self):
+        self.valorA = float(input("Digite o valor A: "))
+        self.valorB = float(input("Digite o valor B: "))
+        self.operacao = input("Digite a operação (+, -, *, /): ")
+
+
+calculadora = Calculadora()
